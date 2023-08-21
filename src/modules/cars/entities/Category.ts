5e1e0b6e158d8c2dcb0569484d44bb9abc9b220f
Ismael Souza <ismaelfernandesz@outlook.com>
2023-08-21 20:09:32 -0300

    Inicio de implementacao do banco de dados

--- src/modules/cars/entities/Category.ts
+++ src/modules/cars/entities/Category.ts
@@ -1,7 +1,12 @@
 import { randomUUID } from "crypto";
+import { Column, Entity, PrimaryColumn } from "typeorm";
 
+@Entity("categories")
 class Category {
+    @PrimaryColumn()
     id?: string;
+
+    @Column()
     name: string;
     description: string;
     created_at: Date;
