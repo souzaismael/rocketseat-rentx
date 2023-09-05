3a0cf7ab226b95f0d696a226041c71e9bf242f77
Ismael Souza <ismaelfernandesz@outlook.com>
2023-09-04 22:22:30 -0300

    Implementando testes

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,29 @@
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
+describe("Create Category", () => {
+    beforeEach(() => {
+        categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+        createCategoryUseCase = new CreateCategoryUseCase(
+            categoriesRepositoryInMemory,
+        );
+    });
+
+    it("should be able to create a new category", async () => {
+        const category = {
+            name: "Category Test",
+            description: "Category description test",
+        };
+
+        await createCategoryUseCase.execute(category);
+
+        const categoryCreated = await categoriesRepositoryInMemory.findByName(
+            category.name,
+        );
+
+        expect(categoryCreated).toHaveProperty("id");
+    });
+});
